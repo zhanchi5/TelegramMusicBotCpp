5ce7212070c4ee8248d11a46ec7a848810ec2c28
zhanchi <zhanchi5@protonmail.com>
2017-12-09 00:01:47 +0300

    UPD

--- INSTALL.md
+++ INSTALL.md
@@ -1,7 +1,7 @@
 # Installation guide
 ## Step 1: `Download release of this repository`
   [release.tar.gz](https://github.com/zhanchi5/Course_project/archive/0.0.1.tar.gz)
-  
+
   [release.rar](https://github.com/zhanchi5/Course_project/archive/0.0.1.zip)
 ## Step 2: `Preparation`
   ```
@@ -17,7 +17,7 @@
   ```
 ## Step 3: `Compilation (Linux)`
   ```
-    g++ main.cpp -o telegram_bot --std=c++11 -I/usr/local/include -lTgBot -lboost_system -lboost_iostreams -lssl -lcrypto -lpthread
+    g++ main.cpp -o telegram_bot main.cpp  RequestManager.cpp --std=c++17 -I/usr/local/include -lTgBot -lboost_system -lboost_iostreams -lssl -lcrypto -lpthread -lcurl
   ```
 ## Step 4: `Starting bot (Linux)`
   ```
--- RequestManager.cpp
+++ RequestManager.cpp
@@ -1,18 +1,17 @@
 #include "RequestManager.hpp"
-#include <iostream>
-#include <exception>
 #include <boost/algorithm/string/replace.hpp>
+#include <exception>
+#include <iostream>
 using nlohmann::json;
 
-RequestManager::RequestManager()
-{
-  std::string telegram_api_key = "377205379:AAEmGRAfTEBASf1tE7pevGok0oYLJENUa8Y";
+RequestManager::RequestManager() {
+  std::string telegram_api_key =
+      "377205379:AAFdYigWVjta_zFOSxVAYfnUV05yVcrmVik";
   _pBot = new TgBot::Bot(telegram_api_key);
+
 }
 
-
 RequestManager::~RequestManager() {}
-
 
 int RequestManager::StartRequestHandler() {
 
@@ -26,28 +25,26 @@ int RequestManager::StartRequestHandler() {
       param = message->text.substr(ind + 1, message->text.length() - ind - 1);
     }
     // TODO: обработать param на предмет nullptr
-    RequestHandle(message, command, param);
+      RequestHandle(message, command, param);
   });
 
   try {
     printf("Bot username: %s\n", _pBot->getApi().getMe()->username.c_str());
 
     TgBot::TgLongPoll longPoll(*_pBot);
-    while (!sigintGot) {
+    while (true) {
       printf("Long poll started\n");
       longPoll.start();
     }
   } catch (std::exception &e) {
     printf("error: %s\n", e.what());
   }
-
-  return 0;
 }
-
 
 int RequestManager::RequestHandle(TgBot::Message::Ptr pMessage,
                                   const std::string &command,
                                   const std::string &param) {
+
 
   int res = 0;
 
@@ -57,30 +54,31 @@ int RequestManager::RequestHandle(TgBot::Message::Ptr pMessage,
 
   std::cout << command << std::endl << param << std::endl;
 
-  if (command == "/top") {
+    if (command == "/start") {
+    res = StartCommand(pMessage);
+  } else if (command == "/top") {
     res = TopCommand(pMessage);
-  }
-  else if (command == "/artist")
-  {
+  } else if (command == "/artist") {
     res = ArtistCommand(pMessage, param);
-  }
-  else if(command == "/video")
-  {
+  } else if (command == "/video") {
     res = VideoCommand(pMessage, param);
-  }
-  else if(command == "/tracks")
-  {
+  } else if (command == "/tracks") {
     res = TrackCommand(pMessage, param);
   }
 
   return res;
 }
 
+int RequestManager::StartCommand(TgBot::Message::Ptr pMessage) {
+  _pBot->getApi().sendMessage(pMessage->chat->id, "Hey, Lets Rocket!");
+  return 0;
+}
 
 int RequestManager::TopCommand(TgBot::Message::Ptr pMessage) {
-  std::string resp = getJSON("http://ws.audioscrobbler.com/2.0/"
-          "?method=chart.gettoptracks&api_key="
-          "34cbc3c5bcdc5f4bbabbd1037f67b870&page=1&format=json");
+  std::string resp =
+      getJSON("http://ws.audioscrobbler.com/2.0/"
+              "?method=chart.gettoptracks&api_key="
+              "34cbc3c5bcdc5f4bbabbd1037f67b870&page=1&format=json");
 
   std::cout << "Parsing file" << std::endl;
   json j = json::parse(resp);
@@ -95,16 +93,15 @@ int RequestManager::TopCommand(TgBot::Message::Ptr pMessage) {
 
   _pBot->getApi().sendMessage(pMessage->chat->id, response);
   _pBot->getApi().sendMessage(pMessage->chat->id, "End of command operation");
+  return 0;
 }
-
 
 int RequestManager::ArtistCommand(TgBot::Message::Ptr pMessage,
                                   const std::string &param) {
 
-
-
   if (param.empty()) {
     // TODO: обработать
+    return -1;
   }
 
   std::string base_url =
@@ -127,7 +124,6 @@ int RequestManager::ArtistCommand(TgBot::Message::Ptr pMessage,
   return 0;
 }
 
-
 int RequestManager::VideoCommand(TgBot::Message::Ptr pMessage,
                                  const std::string &param) {
   if (param.empty()) {
@@ -136,7 +132,6 @@ int RequestManager::VideoCommand(TgBot::Message::Ptr pMessage,
 
   return 0;
 }
-
 
 int RequestManager::TrackCommand(TgBot::Message::Ptr pMessage,
                                  const std::string &param) {
@@ -147,15 +142,14 @@ int RequestManager::TrackCommand(TgBot::Message::Ptr pMessage,
   return 0;
 }
 
-
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                            void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
 }
 
-
 std::string RequestManager::getJSON(const std::string &url) {
-  CURL *curl = nullptr;              // указатель на объект cURL
+  CURL *curl = nullptr;    // указатель на объект cURL
   curl = curl_easy_init(); // запуск "легкого" хэндлера
   CURLcode res;            // объект класса cURLcode
   std::string response;
@@ -164,7 +158,7 @@ std::string RequestManager::getJSON(const std::string &url) {
     struct curl_slist *chunk = nullptr; // список на ноль
     chunk = curl_slist_append(
         chunk,
-        "User-Agent: David"); // кастомный хэдер USERAGENT в список хэдеров
+        "User-Agent: Firefox"); // кастомный хэдер USERAGENT в список хэдеров
     res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER,
                            chunk); // список отправляется в хэдер запроса
     curl_easy_setopt(curl, CURLOPT_URL,
